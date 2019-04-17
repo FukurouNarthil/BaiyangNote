bb02484dc003724138b8108a893890e0db988548
narthil <cucnarthil@163.com>
2019-04-17 10:25:59 +0800

    我的页面前端

--- miniprogram/pages/myPage/myPage.js
+++ miniprogram/pages/myPage/myPage.js
@@ -4,7 +4,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    buttonGroup: [
+      { name: "note", isClick: 0, imageUrl: 'img/note.png', imageUrl_selected: 'img/note_selected.png' },
+      { name: "plan", isClick: 0, imageUrl: 'img/plan.png', imageUrl_selected: 'img/plan_selected.png' },
+      { name: "collection", isClick: 0, imageUrl: 'img/collect.png', imageUrl_selected: 'img/collect_selected.png' },
+      { name: "ranking", isClick: 0, imageUrl: 'img/rank.png', imageUrl_selected: 'img/rank_selected.png' },
+      { name: "analysis", isClick: 0, imageUrl: 'img/analyze.png', imageUrl_selected: 'img/analyze_selected.png'}
+    ],
+    selectedIndex: 0
   },
 
   /**
@@ -39,7 +46,11 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+    var self = this;
+    var str = "buttonGroup[" + self.data.selectedIndex + "].isClick";
+    self.setData({
+      [str]: 0
+    })
   },
 
   /**
@@ -68,6 +79,21 @@ Page({
    */
   onShareAppMessage: function () {
     
-  }
+  },
 
+  // 页面跳转函数
+  redirectToSubPage: function (e) {
+    var self = this;
+    var pageName = e.currentTarget.dataset.buttonname;
+    var idx = e.currentTarget.dataset.idx;
+    var str = "buttonGroup[" + idx + "].isClick";
+    self.setData({
+      [str]: 1,
+      selectedIndex: idx
+    })
+    console.log(self.data)
+    wx.navigateTo({
+      url: '../me/' + pageName + '/' + pageName,
+    })
+  }
 })
