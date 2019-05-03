dd9fef8a2aac00b001468191bdb3445847f0ac7d
Hynn01 <hynn0633@163.com>
2019-05-03 12:42:05 +0800

    可以成功获取到用户的openid

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -7,7 +7,27 @@ App({
     } else {
       wx.cloud.init({
         traceUser: true,
+      }),
+
+      // 调用云函数得到用户的openid
+      wx.cloud.callFunction({
+        name: 'login',
+        data: {},
+        success: res => {
+          console.log('[云函数] [login] user openid: ', res.result.openid)
+          this.globalData.openid = res.result.openid
+          wx.navigateTo({
+            url: '../index/index',
+          })
+        },
+        fail: err => {
+          console.error('[云函数] [login] 调用失败', err)
+          wx.navigateTo({
+            url:  '../deployFunctions/deployFunctions',
+          })
+        }
       })
+
     }
 
     this.globalData = {}
