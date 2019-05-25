baad60f5e480605d40e39e75291a1e48695c4232
Hynn01 <hynn0633@163.com>
2019-05-25 21:11:23 +0800

    数据分析页面初步写好了 还没加动画 先这样

--- miniprogram/pages/me/dataAnalysis/dataAnalysis.js
+++ miniprogram/pages/me/dataAnalysis/dataAnalysis.js
@@ -1,5 +1,68 @@
 // // pages/databaseGuide/databaseGuide.js
 
+const app = getApp()
+
+Page({
+
+  data: {
+    step: 1,
+    counterId: '',
+    openid: '',
+    count: null,
+    queryResult: '',
+  },
+
+  nextStep: function () {
+    console.log("this.data.step:" + this.data.step);    
+    this.setData({
+      step: this.data.step + 1
+    });
+
+  },
+
+  prevStep: function () {
+    console.log("this.data.step:" + this.data.step);    
+    this.setData({
+      step: this.data.step - 1
+    })
+
+  },
+
+  test:function(){
+    console.log("111")
+  },
+
+  goBack:function(){
+    wx.reLaunch({
+      url: '../../myPage/myPage',
+    })
+  }
+
+// goBack: function () {
+//   const pages = getCurrentPages()
+//   if (pages.length === 2) {
+//     wx.navigateBack()
+//   } else if (pages.length === 1) {
+//     wx.redirectTo({
+//       url: '../index/index',
+//     })
+//   } else {
+//     wx.reLaunch({
+//       url: '../index/index',
+//     })
+//   }
+// }
+
+})
+
+
+
+
+
+
+
+
+
 // const app = getApp()
 
 // Page({
@@ -191,70 +254,3 @@
 //   }
 
 // })
-
-// miniprogram/pages/index.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
